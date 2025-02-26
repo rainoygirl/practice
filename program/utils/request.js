999b0292796083156af2243b710e0827d3e11d8d
shengyan <821144561@qq.com>
2025-02-26 17:06:01 +0800

    feat

--- program/utils/request.js
+++ program/utils/request.js
@@ -1,2 +1,30 @@
 // axios 公共配置
 // 基地址
+
+axios.defaults.baseURL = 'http://geek.itheima.net'
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  const token = localStorage.getItem('token')
+// 没有 token 令牌字符串，则强制跳转登录页
+if (!token) {
+  location.href = '../login/index.html'
+}
+return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response;
+  }, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
+
