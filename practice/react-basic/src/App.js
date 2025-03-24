9e58b91d88a719b4436875de5a7bd290bd96d295
shengyan <821144561@qq.com>
2025-03-24 16:51:04 +0800

    init

---
+++ practice/react-basic/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="holy-grail">
+      <header>Header</header>
+      <div class="container">
+        <main class="main-content">Main Content</main>
+        <nav class="left-sidebar">Left Sidebar</nav>
+        <aside class="right-sidebar">Right Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
