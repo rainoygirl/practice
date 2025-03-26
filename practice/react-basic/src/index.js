5089c0e5e2739552e9993c69d577e4089ba8eaa9
shengyan <821144561@qq.com>
2025-03-26 17:01:54 +0800

    init

--- practice/react-basic/src/index.js
+++ practice/react-basic/src/index.js
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App";
 import Test from "./App.test";
 import reportWebVitals from "./reportWebVitals";
-import Holy_Grail from "./Holy Grail";
-import Css_Grid from "./CSS Grid";
-import Full_screen from "./full screen";
+import Holy_Grail from "./0325/Holy Grail";
+import Css_Grid from "./0325/CSS Grid";
+import Full_screen from "./0325/full screen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Holy_Grail /> */}
+    <Holy_Grail />
     {/* <Css_Grid /> */}
-    <Full_screen />
+    {/* <Full_screen /> */}
     {/* <Test /> */}
   </React.StrictMode>
 );
